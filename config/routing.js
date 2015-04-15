d8149a0942fd40834de08b7295154e70aa2f00b2
Michael Cebrian <mcebri01@villanova.edu>
2015-04-15 03:46:11 -0400

    Updated link path

--- config/routing.js
+++ config/routing.js
@@ -2,71 +2,70 @@ var People = require('./models/peopleModel');
 
 var routing = function(router) {
 
-  router.get('/views/:view', function(req, res) {
-    res.render(req.params.view);
-  });
+	router.get('/views/:view', function(req, res) {
+		res.render(req.params.view);
+	});
 
-  router.get('/people/', function(req, res) {
-    People.find({}, function(err, data) {
-      if (err) {
-        return res.status(500).send(err);
-      }
-      return res.send(data);
-    });
-  });
+	router.get('/people/', function(req, res) {
+		People.find({}, function(err, data) {
+			if (err) {
+				return res.status(500).send(err);
+			}
+			return res.send(data);
+		});
+	});
 
-  router.post('/people/', function(req, res) {
-    if (req.body.new) {
-      People.create({
-        name: req.body.name
-      }, function(err, data) {
-        if (err) {
-          return res.status(500).send(err);
-        }
-        People.find({}, function(er, dat) {
-          if (er) {
-            return res.status(500).send(er);
-          }
-          return res.send(dat);
-        });
-      });
-    } else if (req.body.del) {
-      People.remove({
-        _id: req.body._id
-      }, function(err, data) {
-        if (err) {
-          return res.status(500).send(err);
-        }
-        People.find({}, function(er, dat) {
-          if (er) {
-            return res.status(500).send(er);
-          }
-          return res.send(dat);
-        });
-      });
-    } else {
-      console.log("update: " + JSON.stringify(req.body));
-      People.update({
-        _id: req.body._id
-      }, {
-        name: req.body.name
-      }, function(err, data) {
-        if (err) {
-          return res.status(500).send(err);
-        }
-        People.find({}, function(er, dat) {
-          if (er) {
-            return res.status(500).send(er);
-          }
-          return res.send(dat);
-        });
-      });
-    }
-  });
+	router.post('/people/', function(req, res) {
+		if (req.body.new) {
+			People.create({
+				name: req.body.name
+			}, function(err, data) {
+				if (err) {
+					return res.status(500).send(err);
+				}
+				People.find({}, function(er, dat) {
+					if (er) {
+						return res.status(500).send(er);
+					}
+					return res.send(dat);
+				});
+			});
+		} else if (req.body.del) {
+			People.remove({
+				_id: req.body._id
+			}, function(err, data) {
+				if (err) {
+					return res.status(500).send(err);
+				}
+				People.find({}, function(er, dat) {
+					if (er) {
+						return res.status(500).send(er);
+					}
+					return res.send(dat);
+				});
+			});
+		} else {
+			People.findOneAndUpdate({
+				_id: req.body._id
+			}, {
+				name: req.body.name
+			}, function(err, data) {
+				if (err) {
+					return res.status(500).send(err);
+				}
+				People.find({}, function(er, dat) {
+					if (er) {
+						return res.status(500).send(er);
+					}
+					return res.send(dat);
+				});
+			});
+		}
+	});
 
-  router.get('*', function(req, res) {
-    res.render('starter.ejs');
-  });
+	router.get('*', function(req, res) {
+		res.render('starter.ejs');
+	});
 
 };
 
